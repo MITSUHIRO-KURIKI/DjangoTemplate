893a42cbf3b8b35a7637804ba30f5b54056569b6
K.Mitsuhiro <47512020+MITSUHIRO-KURIKI@users.noreply.github.com>
2023-11-02 20:24:34 +0900

    Delete Readme-Sitedata directory

--- Readme-Sitedata/static/templates/common/lib/summernote/load_emoji.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $.ajax({
-        url: 'https://api.github.com/emojis'
-    }).then(function (data) {
-        window.emojis    = Object.keys(data);
-        window.emojiUrls = data;
-    });
-});
