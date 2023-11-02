29d6014328f34d02240341c0fe9214e0cf622154
K.Mitsuhiro <47512020+MITSUHIRO-KURIKI@users.noreply.github.com>
2023-11-02 20:14:58 +0900

    Add files via upload

---
+++ templatetags/common/AccessDict.py
@@ -0,0 +1,13 @@
+from django import template
+from typing import Dict, Union, Any
+
+register = template.Library()
+
+ 
+@register.simple_tag
+def access_dict(dict_:Dict[Union[str, int], Any],
+                arg:Union[str, int]) -> Union[Any, None]:
+    if arg in dict_:
+        return dict_[arg]
+    else:
+        return None
